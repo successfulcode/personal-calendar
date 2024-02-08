c00979afc5fd70b97c9ac94e2e27d8b46e1ca69d
Sergej Mickevic <Sergej@successfulcode.local>
2024-02-08 23:16:52 +0200

    Added fullcalendar

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [],
+  imports: [FullCalendarModule],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
 export class CalendarComponent {
-
+  calendarOptions: CalendarOptions = {
+    plugins: [dayGridPlugin],
+    initialView: 'dayGridMonth',
+    weekends: false,
+    events: [
+      { title: 'Meeting', start: new Date() }
+    ]
+  };
 }
