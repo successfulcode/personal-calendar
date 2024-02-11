3949d68ffeb1983a97923baf2ead40e7dafc46ef
Sergej Mickevic <Sergej@successfulcode.lan>
2024-02-12 00:46:56 +0200

    Updated footer styles

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Signal, computed } from '@angular/core';
+import moment from 'moment';
 
 @Component({
   selector: 'app-footer',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  currenYear: Signal<string> = computed(() => moment().format('YYYY'));
 }
