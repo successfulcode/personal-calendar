90cac0e592c312b9540ec537d4c22c8cda74b090
Sergej Mickevic <Sergej@successfulcode.lan>
2024-02-15 09:15:56 +0200

    Added ids and input tests

--- src/app/components/details-event/details-event.component.spec.ts
+++ src/app/components/details-event/details-event.component.spec.ts
@@ -1,23 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DetailsEventComponent } from './details-event.component';
+import { EventTypes } from 'app/types/enums/event-types.enum';
+import { DatePipe } from '@angular/common';
 
 describe('DetailsEventComponent', () => {
   let component: DetailsEventComponent;
   let fixture: ComponentFixture<DetailsEventComponent>;
+  let datePipe: DatePipe 
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DetailsEventComponent]
+      imports: [DetailsEventComponent],
+      providers: [ DatePipe ]
     })
     .compileComponents();
     
     fixture = TestBed.createComponent(DetailsEventComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    datePipe = TestBed.inject(DatePipe);
+    component.event = {
+      id: 'testId',
+      title: 'Test Event',
+      type: EventTypes.MEETING,
+      start: '2024-02-02T10:00:00',
+      end: '2024-02-02T12:00:00',
+      description: 'Test Description'
+    };
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display event properties correctly', async () => {
+    const element = fixture.nativeElement;
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    const getElement = (id: string): string => element.querySelector(id).textContent
+    expect(getElement('#title')).toContain(component?.event?.title ?? '');
+    expect(getElement('#date')).toContain(datePipe.transform(component?.event?.start, 'yyyy-mm-dd')!);
+    expect(getElement('#start')).toContain(datePipe.transform(component?.event?.start, 'HH:mm')!);
+    expect(getElement('#end')).toContain(datePipe.transform(component?.event?.end, 'HH:mm')!);
+    expect(getElement('#description')).toContain(component?.event?.description!);
   });
 });
